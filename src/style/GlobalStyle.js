5384ea5d99fc899b11dd42200aa90c55a2778fe3
Ahmed Hakim <ahmed@uipia.com>
2019-12-08 15:17:49 +0200

    remove font

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -1,10 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
-import bg from '../img/bg.png';
 
 const GlobalStyle = createGlobalStyle`
   body {
-    font-family: 'Roboto', sans-serif;
-    // background: url('${bg}') top left no-repeat;
+    font-family: sans-serif;
     background-color: ${props => (props.theme.header)};
     @media (min-width: 992px) { 
       background-color: ${props => (props.theme.body)};
