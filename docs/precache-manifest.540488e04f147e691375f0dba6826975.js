fcbf9d8699b83cbf9958128020e4629a2530a406
Ahmed Hakim <ahmed@uipia.com>
2019-12-08 23:29:49 +0200

    adding docs

---
+++ docs/precache-manifest.540488e04f147e691375f0dba6826975.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95663a636e08a8f5b72f3ea17ac8b57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34f86516bee187efc651",
+    "url": "/static/css/main.eb50a357.chunk.css"
+  },
+  {
+    "revision": "bfe3b14c5f0063183ab9",
+    "url": "/static/js/2.50335b98.chunk.js"
+  },
+  {
+    "revision": "34f86516bee187efc651",
+    "url": "/static/js/main.c7168e1e.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  },
+  {
+    "revision": "274f58dd9a666ca65b48c62fbfe8580e",
+    "url": "/static/media/hakim.274f58dd.jpg"
+  },
+  {
+    "revision": "0c55296fb4ee4748af9edd03c35b94e5",
+    "url": "/static/media/icomoon.0c55296f.ttf"
+  },
+  {
+    "revision": "73f71869e2918ac5b6656d508825f1d9",
+    "url": "/static/media/icomoon.73f71869.eot"
+  },
+  {
+    "revision": "ae30e2cf11750d67b205b08a84a921f0",
+    "url": "/static/media/icomoon.ae30e2cf.woff"
+  },
+  {
+    "revision": "e63bc4e103af5533fc3c11abd257e9b0",
+    "url": "/static/media/icomoon.e63bc4e1.svg"
+  }
+]);
