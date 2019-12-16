474d4896195dc50ba7b69d8561fc2915b25509d2
Ahmed Hakim <ahmed@uipia.com>
2019-12-16 22:54:00 +0200

    body padding

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -4,7 +4,9 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-family: sans-serif;
     background-color: ${props => (props.theme.header)};
+    padding-bottom:50px;
     @media (min-width: 600px) { 
+      padding-bottom:0;
       background-color: ${props => (props.theme.body)};
     }
   }
