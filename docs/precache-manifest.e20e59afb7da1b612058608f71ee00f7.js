e411fb812cd4bfa88a0c70ca3f840741b2d8af69
Ahmed Hakim <ahmed@uipia.com>
2019-12-21 14:27:08 +0200

    icons changes

---
+++ docs/precache-manifest.e20e59afb7da1b612058608f71ee00f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3d0b5c37162f9d61807dd3d6cbf8cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e3e9dcb64a47c69fbe0",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "38e5f67f1ad66f65f7f9",
+    "url": "/static/js/2.c2154e0a.chunk.js"
+  },
+  {
+    "revision": "0e3e9dcb64a47c69fbe0",
+    "url": "/static/js/main.e6959246.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  },
+  {
+    "revision": "1eb94d11a4056fe8088d013cc461707c",
+    "url": "/static/media/ahmed-hakim.1eb94d11.pdf"
+  },
+  {
+    "revision": "274f58dd9a666ca65b48c62fbfe8580e",
+    "url": "/static/media/hakim.274f58dd.jpg"
+  },
+  {
+    "revision": "12118dda86a5987f07e31823442e35d8",
+    "url": "/static/media/icon.12118dda.svg"
+  },
+  {
+    "revision": "a8bd80660154fb5f43c1d041ab699aa1",
+    "url": "/static/media/skills.a8bd8066.svg"
+  }
+]);
