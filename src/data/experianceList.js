4cdeb9c44925a7569051721e86b68041fb850e50
Hakim <me@hakim.ninja>
2020-12-29 08:02:17 +0200

    experiance update

--- src/data/experianceList.js
+++ src/data/experianceList.js
@@ -2,62 +2,61 @@ module.exports = [
   {
     companyName: "USYTECH",
     jobTitle: "Lead Front-End Developer",
-    location:"Cairo, Egypt",
+    location: "Cairo, Egypt",
     duration: "Jan 2015 - PRESENT",
-    list:[
-      "Building (confidential Project ) using  <b>React JS</b>, <b>NextJS</b>, <b>Redux</b> and <b>Material-UI</b>.",
-      "Long-term web maintenance in <a target='_blank' href='https://www.eaglerider.com/'>Eaglerider.com</a> site which serves around 300k+ sessions/month.",
-      "Encouraging the team to migrate to Client-side rendering.",
-      "Applying new design and Migrating from server-side rendering to Client-side rendering using <b>React JS</b>, <b>NextJS</b>, <b>Redux</b> and <b>Material-UI</b>.",
-      "Enhancing the performance of pages, regarding google insights the speed rank improved from average 20 to 80 on Mobile and from 40 to 95 on Desktop.",
-      "Leading a team of 4 Developers.",
-      "Refactoring the Mobile Version using <span>Foundation</span>, <span>jQuery</span> and integrating with ruby on rails views."
-    ]
+    list: [
+      "Building (freelancing platform) using  <b>React JS</b>, <b>NextJS</b>, <b>Redux</b> and <b>Material-UI</b>.",
+      "Building and releasing new features in <a target='_blank' href='https://www.eaglerider.com/'>Eaglerider</a>  (bike rentals e-commerce).",
+      "Leading stack migration to <b>React JS</b>, <b>NextJS</b>, <b>Redux</b> and <b>Material-UI</b> from legacy <b>jQuery</b> and ruby server-side rendering",
+      "Planning migration milestones, shipping gradually migrated components to living code, and managing other FE devs efforts to achieve milestones.",
+      "Enhancing checkout pages performance funnel, in these efforts, decreasing page load and perceived loading time, this resulted in increasing Avg session duration by <b>4.20%</b> and decreasing bounce rate by <b>8.79%</b> eventually those wide changes resulted in avg increase of conversion rate by <b>10.70%</b>.",
+      "Leading a team of 5 Developers.",
+    ],
   },
   {
     companyName: "Hindawi Foundation",
     jobTitle: "Senior Front-End Developer",
-    location:"Cairo, Egypt",
+    location: "Cairo, Egypt",
     duration: "Mar 2014 - Jan 2015",
-    list:[
+    list: [
       "Cooperating with the Development team to fix issues in the Epub Generator.",
       "Cooperating with the Mobile team to fix issues in Epub rendering.",
       "Refactoring the structure of Epub box and using oriented style, (As a result, the <span>CSS</span> diminished from 180k to 5k after refactoring, which made the books app render the Epub faster).",
-      "Fixing JS issues in <a target='_blank' href='https://www.hindawi.org/'>Hindawi.org</a> and internal projects."
-    ]
+      "Fixing JS issues in <a target='_blank' href='https://www.hindawi.org/'>Hindawi.org</a> and internal projects.",
+    ],
   },
   {
     companyName: "Softxpert",
     jobTitle: "Front-End Developer",
-    location:"Alexandria, Egypt",
+    location: "Alexandria, Egypt",
     duration: "Jan 2013 - Mar 2014",
-    list:[
+    list: [
       "Enhancing and fixing issues on 4 projects using <span>jQuery</span>.",
       "Translating <a target='_blank' href='https://www.autolina.ch/'>Autolina.ch</a> website to (<span>HTML/CSS</span> and <span>jQuery</span>).",
       "Building an Android mobile app for <span>OCR</span> called smart doc scanner.",
-      "Building a website for smart doc scanner app using <span>HTML/CSS/JS</span> and <span>PHP</span>  then converting it to <span>WordPress</span> theme "
-    ]
+      "Building a website for smart doc scanner app using <span>HTML/CSS/JS</span> and <span>PHP</span>  then converting it to <span>WordPress</span> theme ",
+    ],
   },
   {
     companyName: "GULFSIP",
     jobTitle: "Web Designer/UI Developer (Hyperd)",
-    location:"Alexandria, Egypt",
+    location: "Alexandria, Egypt",
     duration: "Dec 2010 - Dec 2012",
-    list:[
+    list: [
       "Redesigning company website desktop and mobile version, and integrating the markup with <span>PHP (CodeIgniter)</span>.",
       "Designing  a mobile app for <span>iOS</span> and <span>Android</span>.",
-      "Designing and implementing  a wiki to support customer’s troubleshooting with their issues and enhancing SEO rank as all contents of the site were after authentication after that site rank improved used (<span>HTML/CSS/Jquery/PHP/Wordpress</span>)."
-    ]
+      "Designing and implementing  a wiki to support customer’s troubleshooting with their issues and enhancing SEO rank as all contents of the site were after authentication after that site rank improved used (<span>HTML/CSS/Jquery/PHP/Wordpress</span>).",
+    ],
   },
   {
     companyName: "Enozom",
     jobTitle: "Fresh Web Designer/UI Developer (Hyperd)",
-    location:"Alexandria, Egypt",
+    location: "Alexandria, Egypt",
     duration: "Aug 2009 - Nov 2010",
-    list:[
+    list: [
       "Designing  web pages and translated them to markup.",
       "Learning some PHP and used this knowledge in converting the design to 3 WordPress themes.",
-      "Designing and implementing <a target='_blank' href='https://www.pua.edu.eg/'>Pharos University website</a> using (<span>HTML/CSS/Jquery</span>)."
-    ]
-  }
-]
+      "Designing and implementing <a target='_blank' href='https://www.pua.edu.eg/'>Pharos University website</a> using (<span>HTML/CSS/Jquery</span>).",
+    ],
+  },
+];
